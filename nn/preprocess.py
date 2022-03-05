951d4e763a3eabb649bc38289b8b1a9b6c7eb89f
jzhang1198 <jzhang1198@gmail.com>
2022-03-04 19:39:51 -0800

    Finished one_hot_encode_seqs method

--- nn/preprocess.py
+++ nn/preprocess.py
@@ -22,23 +22,35 @@ def one_hot_encode_seqs(seq_arr: List[str]) -> ArrayLike:
             Array of encoded sequences, with each encoding 4x as long as the input sequence
             length due to the one hot encoding scheme for nucleic acids.
 
-            For example, if we encode 
+            For example, if we encode
                 A -> [1, 0, 0, 0]
                 T -> [0, 1, 0, 0]
                 C -> [0, 0, 1, 0]
                 G -> [0, 0, 0, 1]
             Then, AGA -> [1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0]
     """
-    pass
+    one_hot_dict = {'A' : [1, 0, 0, 0],
+                    'T' : [0, 1, 0, 0],
+                    'C' : [0, 0, 1, 0],
+                    'G' : [0, 0, 0, 1]}
+
+    encodings = []
+    for seq in seq_arr: #iterate through sequences in seq_arr
+        one_hot_encoded = [] #generate an empty list to hold one hot encodings for bases
+        for base in seq: #iterate through all bases in seq
+            one_hot_encoded.append(one_hot_dict[base]) #substitute base for one hot encoding
+        encodings.append([item for sublist in one_hot_encoded for item in sublist]) #flatten list of lists
+
+    return encodings
 
 
 def sample_seqs(
         seqs: List[str]
         labels: List[bool]) -> Tuple[List[seq], List[bool]]:
     """
-    This function should sample your sequences to account for class imbalance. 
+    This function should sample your sequences to account for class imbalance.
     Consider this as a sampling scheme with replacement.
-    
+
     Args:
         seqs: List[str]
             List of all sequences.
